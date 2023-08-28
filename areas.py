3466cf80615de05a8e45d53d8f26b49fc75879cb
Chtapodi <x.t.quinn@gmail.com>
2023-08-27 22:44:01 -0700

    implemented preliminary devices

--- areas.py
+++ areas.py
@@ -1,5 +1,6 @@
 import yaml
 import os
+from devices import Device
 
 
 class Area:
@@ -12,16 +13,18 @@ class Area:
 
     def add_child(self, child):
         self.children.append(child)
-        ...
 
     def get_children(self, filter=None):
-        ...
+        return self.children
 
-    def get_value(self, value_id, filter=None):
+    def add_device(self, device):
+        self.devices.append(device)
+
+    def get_value(self, value_id, recurse=True, filter=None):
         # state.get(f'light.{self.name}.brightness')
         ...
 
-    def set_value(self, value_id, filter=None):
+    def set_value(self, value_id, recurse=True, filter=None):
         ...
 
     def _accumulate_values(self, value_id, filter=None):
@@ -33,20 +36,21 @@ class Area:
         """
 
     def pretty_print(self, prefix=""):
-        ret=""
+        ret = ""
         ret += f"{prefix}{self.name}\n"
 
         if len(self.devices) > 0:
-            ret += f"Devices:\n"
+            ret += f"{prefix}::Devices:\n"
 
-            for devices in self.devices:
-                ret += f"\t{device.name}\n"
+            for device in self.devices:
+                device_str = device.pretty_print(prefix + "-")
+                ret += f"{device_str}"
             # ret += f"\n"
 
         elif len(self.children) > 0:
             ret += f"{prefix}>children of {self.name}:\n"
             for child in self.children:
-                child_str=child.pretty_print(prefix+'-')
+                child_str = child.pretty_print(prefix + "-")
                 ret += f"{child_str}"
             # ret += f"\n"
 
@@ -70,13 +74,14 @@ class AreaTree:
         self.root = None
         self.areas = []
 
-    def create_tree(self, yml_file):
-        with open(yml_file, "r") as file:
+    def create_tree(self, area_file, device_file): 
+        with open(area_file, "r") as file:
             area_descriptions = yaml.safe_load(file)
-        print(area_descriptions)
+
+        with open(device_file, "r") as file:
+            device_descriptions = yaml.safe_load(file)
 
         created_areas = []
-
 
         def create_child(parent, child):
             if child not in created_areas:
@@ -87,6 +92,15 @@ class AreaTree:
                 else:
                     print(f"\nERROR: child {child} does not have a description\n")
                     return None
+
+        def create_device(device_name):
+            if device_name in  device_descriptions :
+                device_data = device_descriptions[device_name]
+                device_filters = device_data["filters"]
+                device = Device(device_name, device_filters)
+                return device
+            else :
+                print(f"\nERROR: device {device_name} does not have a description\n")
 
         def create_area(area_name, area_data):
             area = Area(area_name)
@@ -100,12 +114,25 @@ class AreaTree:
                 elif isinstance(children, str):
                     create_child(area, children)
 
+            if "devices" in area_data:
+                devices = area_data["devices"]
+                if isinstance(devices, list):
+                    if len(devices) > 0:
+                        for device in devices:
+                            new_device=create_device(device)
+                            if new_device is not None:
+                                area.devices.append(new_device)
+                elif isinstance(devices, str):
+                    new_device=create_device(devices)
+                    if new_device is not None:
+                        area.devices.append(new_device)
+
             return area
 
-        self.root=create_area("everything", area_descriptions["everything"])
+        self.root = create_area("everything", area_descriptions["everything"])
         created_areas.append(self.root)
 
-        for area in area_descriptions :
+        for area in area_descriptions:
             if area not in created_areas:
                 new_area = create_area(area, area_descriptions[area])
                 created_areas.append(new_area)
@@ -115,4 +142,4 @@ class AreaTree:
 
 print("start")
 tree = AreaTree()
-tree.create_tree("./areas.yml")
+tree.create_tree("./areas.yml", "./devices.yml")
--- devices.py
+++ devices.py
@@ -1,14 +1,26 @@
 
 
 class Device :
-    def __init__(self, id) :
+    def __init__(self, name, filters) :
+        self.name=name
+        self.filters=filters
         self.got_history=[]
         self.set_history=[]
-        ...
+        self.current_value=None
+        
 
-    def get_value(self, value_id, filter) :
+    def get_value(self, value_id, recurse=True, filter=None):
         # state.get(f'light.{self.name}.brightness')
         ...
 
-    def set_value(self, value_id, filter) :
+    def set_value(self, value_id, recurse=True, filter=None):
         ...
+
+    def __repr__(self) :
+        return self.name
+
+    def pretty_print(self, prefix="") :
+        ret=""
+        ret += f"{prefix}-{self.name}\n"
+        ret += f"{prefix}--Current value: {self.current_value}\n"
+        return ret
