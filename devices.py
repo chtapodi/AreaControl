0c82d5bf4e80f585ce57ea7041e694e5e0f78e08
Chtapodi <x.t.quinn@gmail.com>
2023-08-27 19:21:27 -0700

    mocked up devices

---
+++ devices.py
@@ -0,0 +1,12 @@
+
+
+class Device :
+    def __init__(self, id) :
+        ...
+
+    def get_value(self, value_id, filter) :
+        # state.get(f'light.{self.name}.brightness')
+        ...
+
+    def set_value(self, value_id, filter) :
+        ...
